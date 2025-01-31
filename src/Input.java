d49c6a87640e1660ca54a32b38a35c864ec9e161
casandraescobedo <126704586+casandraescobedo@users.noreply.github.com>
2025-01-31 20:49:53 +0000

    Completed the lab

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,29 @@
+import java.util.Scanner;
 
+public class Input{
+
+    public static void main(String[]args){
+        
+        Scanner scanner = new Scanner(System.in);
+
+        String name;
+        int age;
+        double weight;
+        boolean smoke;
+        // User's name
+        System.out.print("Enter your name: ");
+        name = scanner.next();
+        // User's age
+        System.out.print("Enter your age: ");
+        age = scanner.nextInt();
+        // User's weight
+        System.out.print("Enter your weight: ");
+        weight = scanner.nextDouble();
+        // If the user smokes or not
+        System.out.print("Type 'true'/ 'false', Are you a smoker? ");
+        smoke = scanner.nextBoolean();
+
+        System.out.println("The patient's name is " + name + " who is " + age + " years old " + "who weights " + weight + ". Are they a smoker? " + smoke);
+
+    }
+}
